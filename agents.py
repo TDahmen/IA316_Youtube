7931c270413c21265fdd00a4b457a9183f601a21
Paul Chevalier <chaulpevalier@gmail.com>
2020-02-29 17:20:45 +0100

    Deleted useless files

--- agents.py
+++ agents.py
@@ -1,12 +1,9 @@
-import numpy as np
 import random
-import matplotlib.pyplot as plt
 
+import numpy as np
 from scipy.spatial.distance import cosine
 
 from users import User
-from channels import Channel, Video
-from typing import List
 from env import YoutubeEnv
 
 
@@ -17,6 +14,7 @@ def cosine_sim(u, v):
     sim = max(epsilon, sim)
     sim = min(1 - epsilon, sim)
     return sim
+
 
 class Agent:
 
@@ -32,7 +30,7 @@ class Agent:
 
         self.actions = list(env.videos.values())
 
-    def thompson(self, nb_tries, cum_rewards, param = None):
+    def thompson(self, nb_tries, cum_rewards, param=None):
         k = np.shape(nb_tries)[0]
         if param == "beta":
             # Beta prior
@@ -57,7 +55,7 @@ class Agent:
             index = np.where(nb_tries > 0)[0]
             return index[np.argmax(cum_rewards[index] / nb_tries[index])]
 
-    def thompson_sim(self, time_horizon, prior = None):
+    def thompson_sim(self, time_horizon, prior=None):
         k = len(self.actions)
         nb_tries = np.zeros(k, int)
         cum_rewards = np.zeros(k, float)
@@ -76,7 +74,7 @@ class Agent:
         best_action = index[np.argmax(cum_rewards[index] / nb_tries[index])]
         return action_seq, reward_seq
 
-    def eps_greedy_sim(self, time_horizon, prior = None):
+    def eps_greedy_sim(self, time_horizon, prior=None):
         k = len(self.actions)
         nb_tries = np.zeros(k, int)
         cum_rewards = np.zeros(k, float)
@@ -95,7 +93,7 @@ class Agent:
         best_action = index[np.argmax(cum_rewards[index] / nb_tries[index])]
         return action_seq, reward_seq
 
-    def qlearning_sim(self, time_horizon, alpha = 0.7, gamma = 0.3, epsilon = 0.5):
+    def qlearning_sim(self, time_horizon, alpha=0.7, gamma=0.3, epsilon=0.5):
     
         q_table = np.zeros(len(self.actions))
 
@@ -107,9 +105,9 @@ class Agent:
             epochs, penalties, reward, = 0, 0, 0
             
             if random.uniform(0, 1) < epsilon:
-                action = random.sample([i for i in range(0, len(self.actions))], 1)[0] # Explore action space
+                action = random.sample([i for i in range(0, len(self.actions))], 1)[0]  # Explore action space
             else:
-                action = np.argmax(q_table) # Exploit learned values
+                action = np.argmax(q_table)  # Exploit learned values
             reward = self.user.watch(self.actions[action])
             if self.env.evolutive:
                 self.env.update(self.user, self.actions[action], reward)
@@ -139,7 +137,6 @@ class Agent:
                 best_reward = rew
         return best_action, best_reward
 
-
     def get_best_action_sim(self):
         best_action, best_sim = (0, 0)
         for i in range(len(self.actions)):
@@ -149,8 +146,8 @@ class Agent:
                 best_sim = sim
         return best_action, best_sim
 
-## Fonctions outil
 
+# Utils
 def get_regret(reward_seq, best_actions, best_reward):
     time_horizon = len(reward_seq)
     regret = np.zeros(time_horizon, float)
@@ -158,30 +155,3 @@ def get_regret(reward_seq, best_actions, best_reward):
     for t in range(time_horizon):
         regret[t] = best_reward - reward_seq[t]
     return np.cumsum(regret), precision
-
-def show_metrics(metrics, titles, time_horizon):
-
-    fig, (ax1, ax2) = plt.subplots(1,2,figsize=(12, 4))
-
-    ax1.set_xlabel('Time')
-    ax1.set_ylabel('Regret')
-    ax1.title.set_text(titles[0])
-    ax1.plot(range(time_horizon),metrics[0], color = 'b')
-
-    ax2.set_xlabel('Time')
-    ax2.set_ylabel('Precision')
-    ax2.set_ylim(-0.02,1.02)
-    ax2.title.set_text(titles[1])
-    ax2.plot(range(time_horizon),metrics[1], color = 'b')
-    plt.show()
-
-
-
-
-
-
-
-
-
-
-
